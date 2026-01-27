3e4fd0d6293d6a8f20152cd720a6a710f2fb6a03
obamwonyi <developer.oduwa@gmail.com>
2026-01-27 16:16:48 +0100

    Implemented the mobile version

--- src/pages/Screens/LoginMobile.tsx
+++ src/pages/Screens/LoginMobile.tsx
@@ -1,10 +1,46 @@
-export default function LoginMobile()
-{
+import { useState } from "react";
+import LoginImage from "../../assets/login/login_image.svg?react";
+import LogoImage from "../../assets/login/lendsqr_logo.svg?react";
+import "../../styles/login-mobile.scss";
 
-    return(
-        <div>
-            <h1>This is the Login For Mobile</h1>
-        </div>
-    )
+export default function LoginMobile() {
+  const [showPassword, setShowPassword] = useState(false);
 
+  return (
+    <div className="login-mobile">
+      <div className="login-mobile__header">
+        <LogoImage />
+      </div>
+
+      <div className="login-mobile__image">
+        <LoginImage />
+      </div>
+
+      <div className="login-mobile__form">
+        <h1>Welcome!</h1>
+        <p>Enter details to login.</p>
+
+        <form>
+          <input type="email" placeholder="Email" />
+
+          <div className="password-field">
+            <input
+              type={showPassword ? "text" : "password"}
+              placeholder="Password"
+            />
+            <span
+              className="password-field__toggle"
+              onClick={() => setShowPassword((prev) => !prev)}
+            >
+              {showPassword ? "HIDE" : "SHOW"}
+            </span>
+          </div>
+
+          <span className="forgot">FORGOT PASSWORD?</span>
+
+          <button type="submit">LOG IN</button>
+        </form>
+      </div>
+    </div>
+  );
 }
